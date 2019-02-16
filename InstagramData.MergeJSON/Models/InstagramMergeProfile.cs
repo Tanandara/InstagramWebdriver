67b3b92e1723d75f72d1e1dfaff813d0972efc07
Tanandara <tanandara2014@gmail.com>
2019-02-16 14:13:18 +0700

    add Merge JSON project

---
+++ InstagramData.MergeJSON/Models/InstagramMergeProfile.cs
@@ -0,0 +1,21 @@
+﻿using InstagramData.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstagramData.MergeJSON.Models
+{
+    public class InstagramMergeProfile
+    {
+        public string URL { get; set; }
+        public string Name { get; set; }
+        public List<InstagramProfile> Followings { get; set; }
+
+        public InstagramMergeProfile()
+        {
+            this.Followings = new List<InstagramProfile>();
+        }
+    }
+}
---
+++ InstagramData.MergeJSON/Program.cs
@@ -0,0 +1,45 @@
+﻿using InstagramData.Core.Models;
+using InstagramData.MergeJSON.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstagramData.MergeJSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var igMergeProfile = new InstagramMergeProfile
+            { URL = "https://www.instagram.com/amyun.u/", Name = "amyun.u" };
+
+            var jsonFiles = Directory.EnumerateFiles(igMergeProfile.Name,"*.json");
+
+            foreach (string file in jsonFiles)
+            {
+                string json = File.ReadAllText(file);
+
+                var instagramFollowing = JsonConvert.DeserializeObject<InstagramProfile>(json);
+
+                igMergeProfile.Followings.Add(instagramFollowing);
+
+                Console.WriteLine("{0}", instagramFollowing.Name);
+            }
+
+            var fileName = string.Format("{0}_Followings.json", igMergeProfile.Name);
+
+            var content = JsonConvert.SerializeObject(igMergeProfile);
+
+            File.WriteAllText(fileName, content);
+
+            Console.WriteLine("Complete");
+
+            Console.ReadLine();
+        }
+    }
+}
